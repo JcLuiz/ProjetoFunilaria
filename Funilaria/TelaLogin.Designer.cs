2e80220c892c6758f2b7c789d7aabc5591f91463
Joao Cicero <jc_ifsp@hotmail.com>
2012-10-14 18:08:36 -0300

    criacao da tela de login

---
+++ Funilaria/TelaLogin.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Funilaria
+{
+    partial class TelaLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelSenha = new System.Windows.Forms.Label();
+            this.buttonLogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxUsuario
+            // 
+            this.txtBoxUsuario.Location = new System.Drawing.Point(96, 126);
+            this.txtBoxUsuario.Name = "txtBoxUsuario";
+            this.txtBoxUsuario.Size = new System.Drawing.Size(152, 20);
+            this.txtBoxUsuario.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(96, 169);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(152, 20);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Location = new System.Drawing.Point(93, 110);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(43, 13);
+            this.labelUsuario.TabIndex = 2;
+            this.labelUsuario.Text = "Usuário";
+            // 
+            // labelSenha
+            // 
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.Location = new System.Drawing.Point(93, 153);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(38, 13);
+            this.labelSenha.TabIndex = 3;
+            this.labelSenha.Text = "Senha";
+            // 
+            // buttonLogar
+            // 
+            this.buttonLogar.Location = new System.Drawing.Point(173, 204);
+            this.buttonLogar.Name = "buttonLogar";
+            this.buttonLogar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogar.TabIndex = 3;
+            this.buttonLogar.Text = "Entrar";
+            this.buttonLogar.UseVisualStyleBackColor = true;
+            this.buttonLogar.Click += new System.EventHandler(this.buttonLogar_Click);
+            // 
+            // TelaLogin
+            // 
+            this.AcceptButton = this.buttonLogar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonLogar);
+            this.Controls.Add(this.labelSenha);
+            this.Controls.Add(this.labelUsuario);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtBoxUsuario);
+            this.MaximizeBox = false;
+            this.Name = "TelaLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TelaLogin";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TelaLogin_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelSenha;
+        private System.Windows.Forms.Button buttonLogar;
+    }
+}
---
+++ Funilaria/TelaLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Funilaria
+{
+    public partial class TelaLogin : Form
+    {
+        public TelaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogar_Click(object sender, EventArgs e)
+        {
+            // Entra no formulario principal
+            if (txtBoxUsuario.Text == "teste" && txtSenha.Text == "teste")
+            {
+                TelaPrincipal frm = new TelaPrincipal();
+                frm.Show();
+                this.Visible = false;
+            }
+        }
+
+        private void TelaLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+        }
+    }
+}
